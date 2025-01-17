4a9266d445ee194344c1bba8d97a0dcce7049290
Leticia de Andrade <leticia_andrade94@hotmail.com>
2025-01-17 19:43:40 -0300

    refact: apply hexagonal arch

--- src/drivers/web/paymentController.ts
+++ src/drivers/web/paymentController.ts
@@ -1,305 +1,51 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Router, Request, Response } from 'express'
 import { PaymentUseCase } from '../../useCases/payment'
 
 export class PaymentController {
     private routes: Router
+
     constructor(private PaymentUseCase: PaymentUseCase) {
         this.routes = Router()
     }
 
     setupRoutes() {
         this.routes.post('/', this.createPayment.bind(this))
-        this.routes.get('/:id', this.getPayment.bind(this))
-        this.routes.post('/receiver', this.paymentReceiver.bind(this))
-        this.routes.get('/:id/check', this.checkPaymentStatus.bind(this))
+        this.routes.get('/:paymentId', this.getPayment.bind(this))
+        this.routes.post('/webhook', this.paymentWebhook.bind(this))
         return this.routes
     }
 
-    /**
-     * @swagger
-     * /payment:
-     *   post:
-     *     summary: Cria um link de pagamento
-     *     tags: [Payment]
-     *     description: Cria um novo link de pagamento com base nos dados fornecidos.
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               order:
-     *                 type: object
-     *                 properties:
-     *                   idOrder:
-     *                     type: string
-     *                     example: "12345"
-     *                   idClient:
-     *                     type: string
-     *                     example: "67890"
-     *                   cpf:
-     *                     type: string
-     *                     example: "123.456.789-00"
-     *                   name:
-     *                     type: string
-     *                     example: "João Silva"
-     *                   email:
-     *                     type: string
-     *                     example: "joao.silva@example.com"
-     *                   status:
-     *                     type: string
-     *                     example: "WAITING_PAYMENT"
-     *                   value:
-     *                     type: number
-     *                     example: 100.00
-     *                   itens:
-     *                     type: array
-     *                     items:
-     *                       type: object
-     *                       properties:
-     *                         id:
-     *                           type: string
-     *                           example: "123"
-     *                         name:
-     *                           type: string
-     *                           example: "Produto Exemplo"
-     *                         price:
-     *                           type: number
-     *                           example: 50.00
-     *                         quantity:
-     *                           type: number
-     *                           example: 2
-     *               total:
-     *                 type: number
-     *                 example: 100.00
-     *     responses:
-     *       '201':
-     *         description: Link de pagamento criado com sucesso.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 id:
-     *                   type: string
-     *                   example: "12345"
-     *       '500':
-     *         description: Erro interno ao criar o link de pagamento.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   example: "Failed to create payment link"
-     */
-    async createPayment(req: Request, res: Response) {
+    public async createPayment(req: Request, res: Response): Promise<void> {
         try {
-            const result = await this.PaymentUseCase.createPayment(req.body)
-            res.status(201).json(result)
+            const payment = req.body
+            const result = await this.PaymentUseCase.createPayment(payment)
+            res.status(200).json(result)
         } catch (error) {
-            res.status(500).json({ error: 'Failed to create payment link' })
+            console.log('Failed to create payment link', error)
+            res.status(400).json('Failed to create payment link')
         }
     }
 
-    /**
-     * @swagger
-     * /payment/{id}:
-     *   get:
-     *     summary: Obtém os detalhes de um pagamento
-     *     tags: [Payment]
-     *     description: Recupera as informações de um pagamento com base no ID fornecido.
-     *     parameters:
-     *       - name: id
-     *         in: path
-     *         required: true
-     *         description: O ID do pagamento que deve ser recuperado.
-     *         schema:
-     *           type: string
-     *           example: "123454567891011"
-     *     responses:
-     *       '200':
-     *         description: Detalhes do pagamento encontrados com sucesso.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 order:
-     *                   type: object
-     *                   properties:
-     *                     idOrder:
-     *                       type: string
-     *                       example: "12345"
-     *                     idClient:
-     *                       type: string
-     *                       example: "67890"
-     *                     cpf:
-     *                       type: string
-     *                       example: "123.456.789-00"
-     *                     name:
-     *                       type: string
-     *                       example: "João Silva"
-     *                     email:
-     *                       type: string
-     *                       example: "joao.silva@example.com"
-     *                     idPayment:
-     *                       type: string
-     *                       example: "12345678910"
-     *                     status:
-     *                       type: string
-     *                       example: "RECEIVED"
-     *                     value:
-     *                       type: number
-     *                       example: 100.00
-     *                     itens:
-     *                       type: array
-     *                       items:
-     *                         type: object
-     *                         properties:
-     *                           id:
-     *                             type: string
-     *                             example: "123"
-     *                           name:
-     *                             type: string
-     *                             example: "Produto Exemplo"
-     *                           price:
-     *                             type: number
-     *                             example: 50.00
-     *                           quantity:
-     *                             type: number
-     *                             example: 2
-     *                 paymentId:
-     *                   type: string
-     *                   example: "12345678910"
-     *                 paymentLink:
-     *                   type: string
-     *                   example: "http://payment.link/12345"
-     *                 status:
-     *                   type: string
-     *                   example: "PAID"
-     *                 total:
-     *                   type: number
-     *                   example: 100.00
-     *       '404':
-     *         description: Pagamento não encontrado.
-     *       '500':
-     *         description: Erro inesperado ao recuperar o pagamento.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   example: "An unexpected error occurred"
-     */
-    public async getPayment(req: Request, res: Response) {
+    public async getPayment(req: Request, res: Response): Promise<void> {
         try {
-            const paymentId = req.params.id
-            const payment = await this.PaymentUseCase.getPayment(paymentId)
-            if (payment) {
-                res.status(200).json(payment)
-                return
-            }
-            res.status(404).send('Payment not found')
+            const { paymentId } = req.params
+            const result = await this.PaymentUseCase.getPayment(paymentId)
+            res.status(200).json(result)
         } catch (error) {
-            res.status(500).json({ error: 'An unexpected error occurred' })
+            console.log('Failed to get payment', error)
+            res.status(400).json('Failed to get payment')
         }
     }
 
-    async getPaymentData(merchantUrl: string, accessDataToken: string) {
-        const response = await fetch(merchantUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: accessDataToken,
-            },
-        })
-        const result = (await response.json()) as {
-            external_reference: string
-            order_status: string
-        }
-        return {
-            orderId: result.external_reference,
-            status: result.order_status,
-        }
-    }
-
-    async paymentReceiver(req: Request, res: Response) {
-        console.log('PARAMS', req.params)
-        console.log('BODY', req.body)
-
-        const token =
-            'Bearer TEST-1824214370596611-011710-800a45987428cdff374d5c90415c07bb-2211997372'
-        if (req.body.topic === 'merchant_order') {
-            const paymentData = await this.getPaymentData(
-                req.body.resource,
-                token
-            )
-
-            if (paymentData.orderId) {
-                await this.PaymentUseCase.updatePaymentStatus(
-                    paymentData.orderId,
-                    paymentData.status
-                )
-            }
-        }
-
-        res.status(200).send('Dados recebidos com sucesso')
-    }
-
-    /**
-     * @swagger
-     * /payment/{id}/status:
-     *   get:
-     *     summary: Verifica o status de um pagamento
-     *     tags: [Payment]
-     *     description: Recupera o status de um pagamento com base no ID fornecido.
-     *     parameters:
-     *       - name: id
-     *         in: path
-     *         required: true
-     *         description: O ID do pagamento para verificar o status.
-     *         schema:
-     *           type: string
-     *           example: "1234567891011"
-     *     responses:
-     *       '200':
-     *         description: Status do pagamento recuperado com sucesso.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 status:
-     *                   type: string
-     *                   example: "PAID"
-     *       '404':
-     *         description: Pagamento não encontrado.
-     *       '500':
-     *         description: Falha ao verificar o status do pagamento.
-     *         content:
-     *           application/json:
-     *             schema:
-     *               type: object
-     *               properties:
-     *                 error:
-     *                   type: string
-     *                   example: "Failed to check payment status"
-     */
-    async checkPaymentStatus(req: Request, res: Response) {
+    public async paymentWebhook(req: Request, res: Response): Promise<void> {
         try {
-            const paymentId = req.params.id
-
-            const paymentStatus =
-                await this.PaymentUseCase.checkPaymentStatus(paymentId)
-            res.status(200).json(paymentStatus)
+            const webhookData = req.body
+            console.log('webhookData', webhookData)
+            await this.PaymentUseCase.handlePaymentWebhook(webhookData)
+            res.status(200).send('Webhook processed successfully')
         } catch (error) {
-            res.status(500).json({ error: 'Failed to check payment status' })
+            console.log('Failed to process webhook info', error)
+            res.status(400).json('Failed to process webhook info')
         }
     }
 }
--- src/useCases/payment.ts
+++ src/useCases/payment.ts
@@ -1,131 +1,50 @@
-import QRCode from 'qrcode'
-import { PAYMENT_STATUS } from '../constants/payment'
-import { Order } from '../domain/entities/order'
-import { Payment } from '../domain/entities/payment'
-import { Product } from '../domain/entities/product'
-import { OrderRepository } from '../domain/interface/orderRepository'
 import { PaymentRepository } from '../domain/interface/paymentRepository'
+import { OrderRepository } from '../domain/interface/orderRepository'
+import { Payment } from '../domain/entities/payment'
+import { MercadoPagoController } from '../drivers/web/mercadoPagoController'
+import { PAYMENT_STATUS } from '../constants/payment'
 
 export class PaymentUseCase {
+    private paymentRepository: PaymentRepository
+    private orderRepository: OrderRepository
+    private mercadoPagoController: MercadoPagoController
+
     constructor(
-        private paymentRepository: PaymentRepository,
-        private orderRepository: OrderRepository
-    ) {}
-
-    async getUserToken(): Promise<{ token: string; userId: number } | null> {
-        console.log(process.env.MERCADO_PAGO_API)
-        const url = `${process.env.MERCADO_PAGO_API}/oauth/token`
-        const data = {
-            client_secret: process.env.MERCADO_PAGO_CLIENT_SECRET,
-            client_id: process.env.MERCADO_PAGO_CLIENT_ID,
-            grant_type: 'client_credentials',
-            test_token: 'true',
-        }
-
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-            const result = (await response.json()) as {
-                token: string
-                access_token: string
-                token_type: string
-                user_id: number
-            }
-            return {
-                token: `${result.token_type} ${result.access_token}`,
-                userId: result.user_id,
-            }
-        } catch (error) {
-            console.error('Erro na requisição:', error)
-            throw new Error('Failed to fetch QR code token')
-        }
-    }
-
-    async convertQRCodeToImage(qrData: string): Promise<string> {
-        try {
-            return QRCode.toDataURL(qrData, {
-                width: 300,
-                type: 'image/png',
-            })
-        } catch (error) {
-            console.log(error)
-            throw new Error('Erro ao gerar QR code: ' + error)
-        }
-    }
-
-    async generateQRCodeLink(
-        accessData: { token: string; userId: number },
-        order: Order
+        paymentRepository: PaymentRepository,
+        orderRepository: OrderRepository,
+        mercadoPagoController: MercadoPagoController
     ) {
-        const url = `${process.env.MERCADO_PAGO_QR_CODE_API}/${accessData.userId}/pos/Loja1/qrs`
-
-        const data = {
-            external_reference: order.idOrder,
-            title: 'Product order',
-            description: 'FastFood sale',
-            notification_url:
-                'https://7996-152-254-159-137.ngrok-free.app/payment/receiver',
-            total_amount: order.value,
-            items: order.items.map((item: Product) => {
-                return {
-                    sku_number: item.idProduct,
-                    category: 'marketplace',
-                    title: item.name,
-                    description: item.observation,
-                    unit_price: item.unitValue,
-                    unit_measure: 'unit',
-                    total_amount: item.amount * item.unitValue,
-                    quantity: item.amount,
-                }
-            }),
-        }
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: accessData.token,
-                    'X-Idempotency-Key': order.idOrder ?? '',
-                },
-                body: JSON.stringify(data),
-            })
-            const result = (await response.json()) as { qr_data: string }
-            return result
-        } catch (error) {
-            console.error('Erro na requisição:', error)
-            throw new Error('Failed to fetch QR code token')
-        }
-    }
-
-    async generateQRCode(order: Order) {
-        const accessData = await this.getUserToken()
-        if (!accessData?.token || !accessData?.userId) {
-            throw new Error('Failed to fetch QR code token')
-        }
-
-        const qrCodeLink = await this.generateQRCodeLink(accessData, order)
-
-        return this.convertQRCodeToImage(qrCodeLink.qr_data)
+        this.orderRepository = orderRepository
+        this.paymentRepository = paymentRepository
+        this.mercadoPagoController = mercadoPagoController
     }
 
     async createPayment(payment: Payment): Promise<{ id: string }> {
         if (!payment.order.idOrder) {
             throw new Error('Order ID is required')
         }
-
         const existingOrder = await this.orderRepository.getOrder(
             payment.order.idOrder
         )
         if (!existingOrder) {
             throw new Error('Order does not exist')
         }
+        const accessData = await this.mercadoPagoController.getUserToken()
 
-        const QRCodePaymentLink = await this.generateQRCode(payment.order)
+        if (!accessData?.token || !accessData?.userId) {
+            throw new Error('Failed to fetch QR code token')
+        }
+
+        const qrCodeLink = (await this.mercadoPagoController.generateQRCodeLink(
+            accessData,
+            payment.order
+        )) as { qr_data: string }
+
+        const QRCodePaymentLink =
+            await this.mercadoPagoController.convertQRCodeToImage(
+                qrCodeLink.qr_data
+            )
+
         const paymentCreated = await this.paymentRepository.createPayment({
             ...payment,
             paymentLink: QRCodePaymentLink,
@@ -143,37 +62,38 @@ export class PaymentUseCase {
         return paymentCreated
     }
 
-    async getPayment(id: string): Promise<Payment | null> {
-        return this.paymentRepository.getPayment(id)
+    async getPayment(paymentId: string): Promise<Payment> {
+        const payment = await this.paymentRepository.getPayment(paymentId)
+        if (!payment) {
+            throw new Error('Payment not found')
+        }
+        return payment
     }
 
-    async updatePaymentStatus(orderId: string, paymentStatus: string) {
-        const existingOrder = await this.orderRepository.getOrder(orderId)
-        if (!existingOrder) {
-            throw new Error('Order does not exist')
+    async handlePaymentWebhook(webhookData: {
+        resource: string
+        topic: string
+    }): Promise<void> {
+        if (webhookData.topic !== 'merchant_order') return
+
+        const mercadoPagoData =
+            await this.mercadoPagoController.getPaymentStatus(
+                webhookData.resource
+            )
+
+        const paymentId = mercadoPagoData.id
+        const paymentStatus = mercadoPagoData.status
+
+        const payment = await this.paymentRepository.getPayment(paymentId)
+        if (!payment) {
+            throw new Error('Payment not found')
         }
 
-        await this.orderRepository.updateOrderStatus(orderId, paymentStatus)
-        return this.paymentRepository.updatePaymentStatus(
-            orderId,
+        await this.paymentRepository.updatePaymentStatus(
+            paymentId,
             paymentStatus
         )
-    }
 
-    async checkPaymentStatus(id: string): Promise<{ status: string } | null> {
-        const resultStatusPayment =
-            await this.paymentRepository.checkPaymentStatus(id)
-        if (resultStatusPayment?.status === PAYMENT_STATUS.PAID) {
-            const paidOrder = await this.getPayment(id)
-            if (paidOrder?.order?.idOrder) {
-                this.orderRepository.updatePayment(
-                    paidOrder?.order?.idOrder,
-                    id
-                )
-                return resultStatusPayment
-            }
-            throw new Error('Order not found')
-        }
-        return resultStatusPayment
+        await this.orderRepository.updateOrderStatus(paymentId, paymentStatus)
     }
 }

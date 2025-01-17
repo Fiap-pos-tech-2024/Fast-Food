a85d4b9405edaa1c95e17801ca7359b420982d0a
Leticia de Andrade <leticia_andrade94@hotmail.com>
2025-01-17 19:43:40 -0300

    refact: apply hexagonal arch

--- src/domain/interface/paymentRepository.ts
+++ src/domain/interface/paymentRepository.ts
@@ -3,6 +3,5 @@ import { Payment } from '../entities/payment'
 export interface PaymentRepository {
     getPayment(id: string): Promise<Payment | null>
     createPayment(payment: Payment): Promise<{ id: string }>
-    checkPaymentStatus(id: string): Promise<{ status: string } | null>
     updatePaymentStatus(id: string, status: string): Promise<void>
 }
--- src/drivers/database/paymentModel.ts
+++ src/drivers/database/paymentModel.ts
@@ -1,9 +1,9 @@
 import { ObjectId } from 'mongodb'
 import { MongoConnection } from '../../config/mongoConfig'
+import { PaymentRepository } from '../../domain/interface/paymentRepository'
 import { Payment } from '../../domain/entities/payment'
-import { PAYMENT_STATUS } from '../../constants/payment'
 
-export class MongoPaymentRepository {
+export class MongoPaymentRepository implements PaymentRepository {
     private collection = 'payment'
     private mongoConnection: MongoConnection
 
@@ -19,12 +19,8 @@ export class MongoPaymentRepository {
         const db = await this.getDb()
         const payments = await db.collection(this.collection).insertOne({
             _id: new ObjectId(payment.paymentId ?? ''),
-            order: payment.order,
-            paymentLink: payment.paymentLink,
-            status: payment.status,
-            total: payment.total,
+            ...payment,
         })
-
         return { id: payments.insertedId.toString() }
     }
 
@@ -52,28 +48,11 @@ export class MongoPaymentRepository {
         const db = await this.getDb()
         const dbCollection = db.collection(this.collection)
         const query = { _id: new ObjectId(paymentId) }
-        console.log('AQUI', paymentId, status)
         await dbCollection.updateOne(query, {
             $set: {
                 status,
                 'order.status': status,
             },
         })
-    }
-
-    async checkPaymentStatus(
-        paymentId: string
-    ): Promise<{ status: string } | null> {
-        const db = await this.getDb()
-        const payment = await db
-            .collection(this.collection)
-            .findOne({ _id: new ObjectId(paymentId) })
-
-        if (!payment) {
-            throw new Error('Payment not found')
-        }
-
-        // Fake payment success
-        return { status: PAYMENT_STATUS.PAID }
     }
 }
